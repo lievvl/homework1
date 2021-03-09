6feda5e36b657c35a5cf0b693afcee620e24f7dc
lievvl <37303364+lievvl@users.noreply.github.com>
2021-03-09 16:14:53 +0300

    Update geometry.h
    fixed 3.0

--- homework1/geometry.h
+++ homework1/geometry.h
@@ -1,104 +1,115 @@
 #pragma once
-#include <iostream>
 #include <cmath>
+
+const double ER = 1e-9;
+const double pi = 3.1415926535;
 
 class Point {
 private:
-    int x, y;
+    double x_, y_;
 
 public:
-    Point() {
-        this->x = 0;
-        this->y = 0;
+    Point(int a = 0, int b = 0) :
+        x_(a), y_(b)
+    {}
+
+    Point(const Point& other) {
+        x_ = other.x_;
+        y_ = other.y_;
     }
 
-    Point(int a, int b) {
-        this->x = a;
-        this->y = b;
-    }
+    double getX() const { return x_; }
+    void setX(int x) { x_ = x; return; }
 
-    int getX() const { return x; }
-    void setX(int a) { x = a; return; }
+    double getY() const { return y_; }
+    void setY(int y) { y_ = y; return; }
 
-    int getY() const { return y; }
-    void setY(int a) { x = a; return; }
+    Point& operator=(const Point& other) {
+        if (this == &other)
+            return *this;
 
-    Point operator= (const Point obj) {
-        this->x = obj.x;
-        this->y = obj.y;
+        x_ = other.x_;
+        y_ = other.y_;
         return *this;
     }
 };
 
 class PolygonalChain {
 protected:
-    Point* arr;
-    int num;
+    Point* arrayOfPoints_;
+    size_t size_;
 
 public:
-    PolygonalChain() {
-        num = 0;
-        arr = nullptr;
+    PolygonalChain(size_t size = 0, Point* mas = nullptr) {
+        size_ = size;
+        arrayOfPoints_ = new Point[size];
+        for (size_t i = 0; i < size; i++)
+            arrayOfPoints_[i] = mas[i];
     }
 
-    PolygonalChain(int n, Point* mas) {
-        num = n;
-        arr = new Point[n];
-        for (int i = 0; i < n; i++)
-            arr[i] = mas[i];
+    PolygonalChain(const PolygonalChain& other) {
+        size_ = other.size_;
+        arrayOfPoints_ = new Point[size_];
+        for (size_t i = 0; i < size_; i++)
+            arrayOfPoints_[i] = other.arrayOfPoints_[i];
     }
 
-    int getN() const { return num; }
-    Point getPoint(int n) const { return arr[n]; }
+    virtual  ~PolygonalChain() {
+        delete[] arrayOfPoints_;
+    }
 
-    double perimeter() const {
+    int getN() const { return size_; }
+    Point getPoint(int n) const { return arrayOfPoints_[n]; }
+
+    virtual double perimeter() const {
         double p = 0;
-        for (int i = 0; i < num - 1; i++)
-            p += sqrt((arr[i].getX() - arr[i + 1].getX()) * (arr[i].getX() - arr[i + 1].getX()) + (arr[i].getY() - arr[i + 1].getY()) * (arr[i].getY() - arr[i + 1].getY()));
+        for (size_t i = 0; i < size_ - 1; i++)
+            p += sqrt((arrayOfPoints_[i].getX() - arrayOfPoints_[i + 1].getX())
+                * (arrayOfPoints_[i].getX() - arrayOfPoints_[i + 1].getX())
+                + (arrayOfPoints_[i].getY() - arrayOfPoints_[i + 1].getY())
+                * (arrayOfPoints_[i].getY() - arrayOfPoints_[i + 1].getY()));
         return p;
+    }
+
+    PolygonalChain& operator=(const PolygonalChain& other) {
+        if (this == &other)
+            return *this;
+
+        size_ = other.size_;
+        for (size_t i = 0; i < size_; i++)
+            arrayOfPoints_[i] = other.arrayOfPoints_[i];
+        return *this;
     }
 };
 
 class ClosedPolygonalChain : public PolygonalChain {
 public:
-    ClosedPolygonalChain() {
-        num = 0;
-        arr = nullptr;
-    }
+    //fixed constructor from base class
+    ClosedPolygonalChain(size_t size = 0, Point* arrayOfPoints = nullptr) :
+        PolygonalChain(size, arrayOfPoints)
+    {}
 
-    ClosedPolygonalChain(int n, Point* mas) {
-        num = n + 1;
-        arr = new Point[n + 1];
-        for (int i = 0; i < n; i++)
-            arr[i] = mas[i];
-        arr[n] = mas[0];
+    double perimeter() const {
+        return PolygonalChain::perimeter() + sqrt((arrayOfPoints_[0].getX() - arrayOfPoints_[size_ - 1].getX())
+            * (arrayOfPoints_[0].getX() - arrayOfPoints_[size_ - 1].getX())
+            + (arrayOfPoints_[0].getY() - arrayOfPoints_[size_ - 1].getY())
+            * (arrayOfPoints_[0].getY() - arrayOfPoints_[size_ - 1].getY()));
     }
-
-    int getN() const { return num - 1; }
 };
 
 class Polygon : public ClosedPolygonalChain {
 public:
-    Polygon() {
-        num = 0;
-        arr = nullptr;
-    }
+    Polygon(size_t n = 0, Point* arrayOfPoints = nullptr) :
+        ClosedPolygonalChain(n, arrayOfPoints)
+    {}
 
-    Polygon(int n, Point* mas) {
-        num = n + 1;
-        arr = new Point[n + 1];
-        for (int i = 0; i < n; i++)
-            arr[i] = mas[i];
-        arr[n] = mas[0];
-    }
-
-    double area() const {
-        int a = 0;
-        for (int i = 0; i < num - 2; i++)
-            a += arr[i].getX() * arr[i + 1].getY();
-        int b = 0;
-        for (int i = 0; i < num - 2; i++)
-            b += arr[i + 1].getX() * arr[i].getY();
+    virtual double area() const {
+        double a = 0;
+        for (size_t i = 0; i < size_ - 1; i++)
+            a += arrayOfPoints_[i].getX() * arrayOfPoints_[i + 1].getY();
+        double b = 0;
+        for (size_t i = 0; i < size_ - 1; i++)
+            b += arrayOfPoints_[i + 1].getX() * arrayOfPoints_[i].getY();
 
         return abs((a - b) / 2);
     }
@@ -106,51 +117,72 @@ public:
 
 class Triangle : public Polygon {
 public:
-    Triangle(int n, Point* mas) {
-        num = n + 1;
-        arr = new Point[n + 1];
-        for (int i = 0; i < n; i++)
-            arr[i] = mas[i];
-        arr[n] = mas[0];
-    }
+    Triangle(int n, Point* arrayOfPoints) :
+        Polygon(n, arrayOfPoints)
+    {}
 
+    //fixed without sqrt
     bool hasRightAngle() const {
-        double a = sqrt((arr[0].getX() - arr[1].getX()) * (arr[0].getX() - arr[1].getX()) + (arr[0].getY() - arr[1].getY()) * (arr[0].getY() - arr[1].getY()));
-        double b = sqrt((arr[0].getX() - arr[2].getX()) * (arr[0].getX() - arr[2].getX()) + (arr[0].getY() - arr[2].getY()) * (arr[0].getY() - arr[2].getY()));
-        double c = sqrt((arr[1].getX() - arr[2].getX()) * (arr[1].getX() - arr[2].getX()) + (arr[1].getY() - arr[2].getY()) * (arr[1].getY() - arr[2].getY()));
-        if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b)
-            return true;
-        else
-            return false;
+        double a = (arrayOfPoints_[0].getX() - arrayOfPoints_[1].getX())
+            * (arrayOfPoints_[0].getX() - arrayOfPoints_[1].getX())
+            + (arrayOfPoints_[0].getY() - arrayOfPoints_[1].getY())
+            * (arrayOfPoints_[0].getY() - arrayOfPoints_[1].getY());
+        double b = (arrayOfPoints_[0].getX() - arrayOfPoints_[2].getX())
+            * (arrayOfPoints_[0].getX() - arrayOfPoints_[2].getX())
+            + (arrayOfPoints_[0].getY() - arrayOfPoints_[2].getY())
+            * (arrayOfPoints_[0].getY() - arrayOfPoints_[2].getY());
+        double c = (arrayOfPoints_[1].getX() - arrayOfPoints_[2].getX())
+            * (arrayOfPoints_[1].getX() - arrayOfPoints_[2].getX())
+            + (arrayOfPoints_[1].getY() - arrayOfPoints_[2].getY())
+            * (arrayOfPoints_[1].getY() - arrayOfPoints_[2].getY());
+        //fixed you have constant
+        //fixed return expression
+        return (abs(a + b - c) <= ER
+            || abs(b + c - a) <= ER
+            || abs(c * c + a - b) <= ER);
     }
 };
 
 class Trapezoid : public Polygon {
 public:
-    Trapezoid(int n, Point* mas) {
-        num = n + 1;
-        arr = new Point[n + 1];
-        for (int i = 0; i < n; i++)
-            arr[i] = mas[i];
-        arr[n] = mas[0];
-    }
+    Trapezoid(int n, Point* arrayOfPoints) :
+        Polygon(n, arrayOfPoints)
+    {}
 
     double height() const {
         double b, d;
-        b = sqrt((arr[1].getX() - arr[2].getX()) * (arr[1].getX() - arr[2].getX()) + (arr[1].getY() - arr[2].getY()) * (arr[1].getY() - arr[2].getY()));
-        d = sqrt((arr[3].getX() - arr[4].getX()) * (arr[3].getX() - arr[4].getX()) + (arr[3].getY() - arr[4].getY()) * (arr[3].getY() - arr[4].getY()));
+        b = sqrt((arrayOfPoints_[1].getX() - arrayOfPoints_[2].getX())
+            * (arrayOfPoints_[1].getX() - arrayOfPoints_[2].getX())
+            + (arrayOfPoints_[1].getY() - arrayOfPoints_[2].getY())
+            * (arrayOfPoints_[1].getY() - arrayOfPoints_[2].getY()));
+        d = sqrt((arrayOfPoints_[3].getX() - arrayOfPoints_[0].getX())
+            * (arrayOfPoints_[3].getX() - arrayOfPoints_[0].getX())
+            + (arrayOfPoints_[3].getY() - arrayOfPoints_[0].getY())
+            * (arrayOfPoints_[3].getY() - arrayOfPoints_[0].getY()));
         double med = (b + d) / 2;
         return this->area() / med;
     }
 };
-
+//fixed area and perimeter
 class RegularPolygon : public Polygon {
 public:
-    RegularPolygon(int n, Point* mas) {
-        num = n + 1;
-        arr = new Point[n + 1];
-        for (int i = 0; i < n; i++)
-            arr[i] = mas[i];
-        arr[n] = mas[0];
+    RegularPolygon(int n, Point* arrayOfPoints) :
+        Polygon(n, arrayOfPoints)
+    {}
+
+    double area() {
+        double side = sqrt((arrayOfPoints_[0].getX() - arrayOfPoints_[1].getX())
+            * (arrayOfPoints_[0].getX() - arrayOfPoints_[1].getX())
+            + (arrayOfPoints_[0].getY() - arrayOfPoints_[1].getY())
+            * (arrayOfPoints_[0].getY() - arrayOfPoints_[1].getY()));
+        return (size_ * side * side) / (4 * tan(pi / size_));
+    }
+
+    double perimeter() {
+        double side = sqrt((arrayOfPoints_[0].getX() - arrayOfPoints_[1].getX())
+            * (arrayOfPoints_[0].getX() - arrayOfPoints_[1].getX())
+            + (arrayOfPoints_[0].getY() - arrayOfPoints_[1].getY())
+            * (arrayOfPoints_[0].getY() - arrayOfPoints_[1].getY()));
+        return size_ * side;
     }
 };
